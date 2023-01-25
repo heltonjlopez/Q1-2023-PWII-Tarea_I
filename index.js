ba7134f591825b1c8b82fa8d25baa523fb18ac13
Helton Lopez <heltonjlopez@gmail.com>
2023-01-24 22:29:52 -0600

    commit inicial

---
+++ index.js
@@ -0,0 +1,12 @@
+const { simplify } = require("mathjs");
+var colors = require('colors');
+
+console.log('Esta es mi tarea, con la cual utilice mathjs');
+
+console.log('Expresiones simplificadas');
+console.log(simplify('5+4/2').toString()); //Esto es igual a 7
+
+
+console.log('Esta tare ha sido hecha por ' + 'Helton'.blue + ' Joan'.red + ' Lopez'.green + ' Martinez'.america);
+// console.log('Este es un texto subrayado amarillo'.underline.bgYellow);
+
